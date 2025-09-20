5d389f216e284ecaab6b6b93287ea9e60277fda2
umul <umulumul169@gmail.com>
2025-09-20 17:36:34 +0900

    Update utils

--- modules/utils.py
+++ modules/utils.py
@@ -6,16 +6,11 @@ def font_to_base64(path):
         return base64.b64encode(f.read()).decode("utf-8")
 
 def inject_fonts():
-    jalnan2_font = font_to_base64("assets/Jalnan2TTF.ttf")
+    # jalnan2_font = font_to_base64("assets/Jalnan2TTF.ttf")
     jalnan_gothic_font = font_to_base64("assets/JalnanGothicTTF.ttf")
 
     st.markdown(f"""
         <style>
-        @font-face {{
-            font-family: 'Jalnan2';
-            src: url(data:font/ttf;base64,{jalnan2_font}) format('truetype');
-            font-weight: 400;
-        }}
         @font-face {{
             font-family: 'JalnanGothic';
             src: url(data:font/ttf;base64,{jalnan_gothic_font}) format('truetype');
